b15e62a4244e177bfff8b73f0f34d714dfaa6897
Carol Gardiner <carolan.gardiner@gmail.com>
2019-01-06 10:51:22 +0000

    Added rakefile functionality

--- datamapper_setup.rb
+++ datamapper_setup.rb
@@ -2,5 +2,4 @@ require './lib/bookmark'
 
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager")
 DataMapper::Logger.new($stdout, :debug)
-DataMapper.auto_upgrade!
 DataMapper.finalize
